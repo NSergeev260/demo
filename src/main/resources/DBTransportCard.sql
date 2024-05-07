5f38e741bab821c7f9ed6d9745723f8893c8b4f4
NSergeev260 <nik.sergeev260@gmail.com>
2024-05-07 15:27:32 +0300

    Separated on two table

--- src/main/resources/DBTransportCard.sql
+++ src/main/resources/DBTransportCard.sql
@@ -1,19 +1,39 @@
-CREATE DATABASE transportCard;
-use transportCard;
+
+CREATE DATABASE Card;
+USE Card;
 
 CREATE TABLE IF NOT EXISTS transportCard(
 cardID VARCHAR(36) NOT NULL,
-userName VARCHAR(255) NOT NULL,
-userSurname VARCHAR(255) NOT NULL,
 balance DECIMAL(8, 2) NOT NULL,
-debitCard VARCHAR(3),
-creditCard VARCHAR(3),
+type VARCHAR(6),
 cutOffBankDept DECIMAL(5, 2) NOT NULL,
-statusCard VARCHAR(255) NOT NULL,
+isBlocked VARCHAR(5) NOT NULL,
 PRIMARY KEY(cardID)
 );
 
-INSERT INTO transportCard(cardID, userName, userSurname, balance, debitCard, creditCard, cutOffBankDept, statusCard) VALUES (UUID(), 'VASYA', 'PUPKIN', 0.0, 'NO', 'YES', 0.0, "Active");
+INSERT INTO transportCard(cardID, balance, type, cutOffBankDept, isBlocked) VALUES (UUID(), 50, 'CREDIT', 100, 'false');
 
 SELECT * FROM transportCard;
 DESCRIBE transportCard;
+
+
+CREATE TABLE IF NOT EXISTS cardHolder(
+cardID VARCHAR(36) NOT NULL,
+documentId VARCHAR(36) NOT NULL,
+PRIMARY KEY(cardID)
+);
+
+INSERT INTO cardHolder (cardID)
+SELECT
+	cardID
+FROM
+	transportCard
+WHERE
+	cardID = transportCard.cardID;
+INSERT INTO cardHolder(documentId) VALUES (UUID());
+
+SELECT * FROM cardHolder;
+DESCRIBE cardHolder;
+
+ALTER TABLE cardHolder ADD FOREIGN KEY(cardID) REFERENCES transportCard(cardID);
+SELECT * FROM transportCard CROSS JOIN cardHolder ON cardHolder.cardID = transportCard.cardID;
