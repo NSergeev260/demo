2e3cbed481d899d53fa41c1e9ce9c6daac79be00
NSergeev260 <nik.sergeev260@gmail.com>
2024-06-15 16:23:28 +0300

    Emulated of common command with cards(get, delete, insert, update)

--- src/main/java/com/emulator/EmulatorController.java
+++ src/main/java/com/emulator/EmulatorController.java
@@ -1,40 +1,43 @@
 package com.emulator;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.time.Instant;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class EmulatorController {
 
-    private static final String URL = "http://localhost:80/getCard?cardId=106fbd78-1f37-11ef-bd2a-0cd292f91adb";
-    //    private static final String URL = "https://official-joke-api.appspot.com/random_joke";
-    private static final int NUMBER_OF_REQUESTS = 1;
+    private static final String CARD_ID = "106fbd78-1f37-11ef-bd2a-0cd292f91adb";
+    private static final String DELETE_CARD_ID = "369fbccc-e724-433f-860d-db601e17ed3c";
+
+    private static final String UPDATE_CARD_ID = "106fbd78-1f37-11ef-bd2a-0cd292f91adb";
+    private static final String UPDATE_BALANCE = "&balance=" + "2000";
+    private static final String UPDATE_BLOCKED_STATUS = "&isBlocked=" + "false";
+    private static final String UPDATE_DOCUMENT_ID = "&documentId=" + "as";
+
+    private static final String URL_GET_CARD = "http://localhost:80/getCard?cardId=" + CARD_ID;
+    private static final String URL_INSERT_CARD = "http://localhost:80/insertTest";
+    private static final String URL_GET_ALL_CARDS = "http://localhost:80/getCards";
+    private static final String URL_DELETE_CARD = "http://localhost:80/delete?cardId=" + DELETE_CARD_ID;
+    private static final String URL_UPDATE_CARD = "http://localhost:80/update?cardId=" + UPDATE_CARD_ID +
+        UPDATE_BALANCE + UPDATE_BLOCKED_STATUS;
+//    + UPDATE_DOCUMENT_ID;
+    //    private static final int NUMBER_OF_REQUESTS = 1;
+
+
 
     public static void main(String[] args) throws Exception {
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(new URI(URL))
-            .GET()
-            .build();
-        try {
-            HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                System.out.println("Body: " + response.body());
-                System.out.println("\nStatus Code: " + response.statusCode());
-            } else {
-                System.out.println("Something wrong there!");
-            }
-        } catch (IOException e) {
-            System.out.println("Something wrong here!");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("Request method: " + request.method());
+        Requests requests = new Requests();
+
+//        System.out.println("=======GET CARD======");
+//        requests.setGetRequest(URL_GET_CARD);
+//        System.out.println("=======INSERT CARD======");
+//        requests.setPostRequest(URL_INSERT_CARD);
+//        System.out.println("=======GET ALL CARDs======");
+//        requests.setGetRequest(URL_GET_ALL_CARDS);
+        System.out.println("=======DELETE ONE CARD======");
+        requests.setPostRequest(URL_DELETE_CARD);
+
+//        System.out.println("=======UPDATE CARD======");
+//        requests.setPostRequest(URL_UPDATE_CARD);
     }
 }
---
+++ src/main/java/com/emulator/Requests.java
@@ -0,0 +1,55 @@
+package com.emulator;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Slf4j
+public class Requests {
+
+    public void setGetRequest(String URL) throws URISyntaxException {
+
+        HttpRequest request = HttpRequest.newBuilder(new URI(URL))
+            .GET()
+            .build();
+
+        getResponse(request);
+    }
+
+    public void setPostRequest(String URL) throws URISyntaxException {
+
+        HttpRequest request = HttpRequest.newBuilder(new URI(URL))
+            .POST(HttpRequest.BodyPublishers.noBody())
+            .build();
+
+        getResponse(request);
+    }
+
+    private static void getResponse(HttpRequest request) {
+
+        HttpClient client = HttpClient.newHttpClient();
+        try {
+            HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                log.info("Request method: " + request.method());
+                log.info("Status Code: " + response.statusCode());
+                log.info("Body: " + response.body());
+            } else {
+                log.info("Something wrong. StatusCode NOT 200!");
+            }
+
+        } catch (
+            IOException e) {
+            log.info("Something wrong! CATCH block.");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
