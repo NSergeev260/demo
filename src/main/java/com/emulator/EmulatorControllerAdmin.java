b30a7f14ce09109844e573d1208cb9cbf8419c3a
NSergeev260 <nik.sergeev260@gmail.com>
2024-07-21 18:51:46 +0300

    Refactoring URL request

--- src/main/java/com/emulator/EmulatorControllerAdmin.java
+++ src/main/java/com/emulator/EmulatorControllerAdmin.java
@@ -17,52 +17,70 @@ public class EmulatorControllerAdmin {
 
     public void getBlock(String cardId, String terminalId) throws URISyntaxException {
         log.info("=======BLOCK A CARD======");
-        parallelRequestsHttp.postRequest(URL_PATH + "/block" + URL_CARD + cardId +
-            URL_TERMINAL + terminalId);
+        String request = URL_PATH + "/block" + URL_CARD + cardId +
+            URL_TERMINAL + terminalId;
+
+        parallelRequestsHttp.postRequest(request);
     }
 
     public void getUnblock(String cardId, String terminalId) throws URISyntaxException {
         log.info("=======UNBLOCK A CARD======");
-        parallelRequestsHttp.postRequest(URL_PATH + "/unblock" + URL_CARD + cardId +
-            URL_TERMINAL + terminalId);
+        String request = URL_PATH + "/unblock" + URL_CARD + cardId +
+            URL_TERMINAL + terminalId;
+
+        parallelRequestsHttp.postRequest(request);
     }
 
     public String getStatusBlocked(String cardId) throws URISyntaxException {
         log.info("=======IS BLOCKED======");
-        return parallelRequestsHttp.getRequest(URL_PATH + "/isBlocked" + URL_CARD + cardId);
+        String request = URL_PATH + "/isBlocked" + URL_CARD + cardId;
+
+        return parallelRequestsHttp.getRequest(request);
     }
 
     public void getMockData(String numberOfRecords) throws URISyntaxException {
         log.info("=======GET DATA FOR DB======");
-        parallelRequestsHttp.postRequest(URL_PATH + "/generateMockData" +
-            URL_RECORDS + numberOfRecords);
+        String request = URL_PATH + "/generateMockData" +
+            URL_RECORDS + numberOfRecords;
+
+        parallelRequestsHttp.postRequest(request);
     }
 
     public void insertCard() throws URISyntaxException {
         log.info("=======INSERT CARD======");
-        parallelRequestsHttp.postRequest(URL_PATH + "/insertTest");
+        String request = URL_PATH + "/insertTest";
+
+        parallelRequestsHttp.postRequest(request);
     }
 
     public void getCard(String cardId) throws URISyntaxException {
         log.info("=======GET CARD======");
-        parallelRequestsHttp.getRequest(URL_PATH + "/getCard" + URL_CARD + cardId);
+        String request = URL_PATH + "/getCard" + URL_CARD + cardId;
+
+        parallelRequestsHttp.getRequest(request);
     }
 
     public String getAllCards() throws URISyntaxException {
         log.info("=======GET ALL CARDs======");
-        return parallelRequestsHttp.getRequest(URL_PATH + "/getCards");
+        String request = URL_PATH + "/getCards";
+
+        return parallelRequestsHttp.getRequest(request);
     }
 
     public void updateCard(String cardId, String balance, String isBlocked,
                            String document) throws URISyntaxException {
         log.info("=======UPDATE CARD======");
-        parallelRequestsHttp.postRequest(URL_PATH + "/update" + URL_CARD + cardId +
+        String request = URL_PATH + "/update" + URL_CARD + cardId +
             URL_BALANCE + balance + URL_BLOCKED + isBlocked + URL_DOCUMENT +
-            document);
+            document;
+
+        parallelRequestsHttp.postRequest(request);
     }
 
     public void deleteCard(String cardId) throws URISyntaxException {
         log.info("=======DELETE ONE CARD======");
-        parallelRequestsHttp.postRequest(URL_PATH + "/delete" + URL_CARD + cardId);
+        String request = URL_PATH + "/delete" + URL_CARD + cardId;
+
+        parallelRequestsHttp.postRequest(request);
     }
 }
--- src/main/java/com/emulator/EmulatorControllerTerminal.java
+++ src/main/java/com/emulator/EmulatorControllerTerminal.java
@@ -2,7 +2,11 @@ package com.emulator;
 
 import com.finalproject.transport.Transport;
 import lombok.extern.slf4j.Slf4j;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 @Slf4j
 public class EmulatorControllerTerminal {
@@ -18,31 +22,42 @@ public class EmulatorControllerTerminal {
     public void getPay(String cardId, Transport typeOfTransport,
                        String terminalId) throws URISyntaxException {
         log.info("=======PAY MONEY======");
-        parallelRequestsHttp.postRequest(URL_PATH + "/pay" + URL_CARD + cardId +
-            URL_TRANSPORT + typeOfTransport + URL_TERMINAL + terminalId);
+        String request = URL_PATH + "/pay" + URL_CARD + cardId +
+            URL_TRANSPORT + typeOfTransport + URL_TERMINAL + terminalId;
+
+        parallelRequestsHttp.postRequest(request);
+
     }
 
     public void getPut(String cardId, String money,
                        String terminalId) throws URISyntaxException {
         log.info("=======PUT MONEY======");
-        parallelRequestsHttp.postRequest(URL_PATH + "/put" + URL_CARD + cardId +
-            URL_MONEY + money + URL_TERMINAL + terminalId);
+        String request = URL_PATH + "/put" + URL_CARD + cardId +
+            URL_MONEY + money + URL_TERMINAL + terminalId;
+
+        parallelRequestsHttp.postRequest(request);
     }
 
     public void getBalance(String cardId, String terminalId) throws URISyntaxException {
         log.info("=======GET BALANCE======");
-        parallelRequestsHttp.getRequest(URL_PATH + "/balance" + "/" + cardId + "?" +
-            URL_TERMINAL + terminalId);
+        String request = URL_PATH + "/balance" + "/" + cardId + "?" +
+            URL_TERMINAL + terminalId;
+
+        parallelRequestsHttp.getRequest(request);
     }
 
     public void getInfo(String cardId) throws URISyntaxException {
         log.info("=======GET INFO======");
-        parallelRequestsHttp.getRequest(URL_PATH + "/getInfo" + URL_CARD + cardId);
+        String request = URL_PATH + "/getInfo" + URL_CARD + cardId;
+
+        parallelRequestsHttp.getRequest(request);
     }
 
     public String activateCard(String cardType, String terminalId) throws URISyntaxException {
         log.info("=======ACTIVATE(INSERT) CARD======");
-        return parallelRequestsHttp.postRequest(URL_PATH + "/activate" + URL_CARD_TYPE + cardType +
-            URL_TERMINAL + terminalId);
+        String request = URL_PATH + "/activate" + URL_CARD_TYPE + cardType +
+            URL_TERMINAL + terminalId;
+
+        return parallelRequestsHttp.postRequest(request);
     }
 }
